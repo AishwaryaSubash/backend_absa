c5b517c7343bf3105a2aaf0001cea67af4b21647
KE2002 <44949462+KE2002@users.noreply.github.com>
2023-09-29 17:33:11 +0530

    v

--- src/app.service.ts
+++ src/app.service.ts
@@ -104,7 +104,7 @@ export class AppService {
         predictions: predictions,
         details: det,
       };
-      return combinedData[0];
+      return predictions[0];
     } catch (error) {
       console.log('error');
     }
